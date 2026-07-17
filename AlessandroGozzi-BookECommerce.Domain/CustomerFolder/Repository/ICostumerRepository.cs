20c1b7ac01258c8555417a19ba15658687897cc8
AlessandroGozziBlaisePascal <alessandro.gozzi.stud@ispascalcomandini.it>
2026-07-17 19:30:38 +0200

    Finished buyer-customer POV and cart

--- AlessandroGozzi-BookECommerce.Domain/CustomerFolder/Repository/ICostumerRepository.cs
+++ AlessandroGozzi-BookECommerce.Domain/CustomerFolder/Repository/ICostumerRepository.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AlessandroGozzi.BookECommerce.SharedKernel;
+using AlessandroGozzi_BookECommerce.Domain.CustomerFolder.Value_Object;
 
 namespace AlessandroGozzi_BookECommerce.Domain.CustomerFolder.Repository
 {
-    internal class ICostumerRepository
+    public interface ICostumerRepository
     {
+        Task<Result<Customer>> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<Result<Customer>> GetByEmailAsync(Email email, CancellationToken cancellationToken = default);
+        Task<Result> AddAsync(Customer customer, CancellationToken cancellationToken = default);
+        Result Update(Customer customer);
+        Result Delete(Customer customer);
     }
 }
