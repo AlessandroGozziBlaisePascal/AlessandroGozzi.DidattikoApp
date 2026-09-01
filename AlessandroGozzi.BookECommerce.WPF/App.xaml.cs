ab04d728756453b295b1f758706214ddba9c0d09
AlessandroGozziBlaisePascal <alessandro.gozzi.stud@ispascalcomandini.it>
2026-09-01 22:35:02 +0200

    Added XAML code for Auth pages

--- AlessandroGozzi.BookECommerce.WPF/App.xaml.cs
+++ AlessandroGozzi.BookECommerce.WPF/App.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
+using AlessandroGozzi.BookECommerce.Infrastructure;
 using AlessandroGozzi.BookECommerce.Infrastructure.Persistance;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Stripe.BillingPortal;
 
 namespace AlessandroGozzi.BookECommerce.WPF
@@ -12,15 +17,51 @@ namespace AlessandroGozzi.BookECommerce.WPF
     /// </summary>
     public partial class App : System.Windows.Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
+        public static IHost? AppHost { get; private set; }
 
-            using(var context = new DesignTimeDbContextFactory().CreateDbContext(new string[0]))
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddInfrastructureServices(hostContext.Configuration);
+
+                    services.AddSingleton<LoginView>();
+                    services.AddSingleton<PasswordResetView>();
+                    services.AddSingleton<RegistrationView>();
+
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+
+            var initialView = AppHost.Services.GetRequiredService<LoginView>();
+
+            this.MainWindow = initialView;
+            initialView.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            if (AppHost != null)
             {
-                context.Database.Migrate();
+                await AppHost.StopAsync();
+                AppHost.Dispose();
             }
+
+            base.OnExit(e);
         }
     }
+
 
 }
--- AlessandroGozzi.BookECommerce.WPF/RegistrationView.xaml.cs
+++ AlessandroGozzi.BookECommerce.WPF/RegistrationView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,11 +16,11 @@ using System.Windows.Shapes;
 namespace AlessandroGozzi.BookECommerce.WPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logica di interazione per RegistrationView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RegistrationView : Window
     {
-        public MainWindow()
+        public RegistrationView()
         {
             InitializeComponent();
         }
